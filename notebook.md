881ff92c3886de7dd299153ea39a77bddf85ba05
DDXY1230 <2502752288@qq.com>
2020-09-25 14:51:17 +0800

    htmlwebpackplugin

--- notebook.md
+++ notebook.md
@@ -1,3 +1,4 @@
 ## 笔记
 > npm init -y  初始化package.jsons
 > npm i -D webpack  在开发环境安装webpack
+> npm i -D webpack@3  在开发环境安装webpack 版本3
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 module.exports = {
     mode: 'development',
@@ -5,5 +6,11 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main_dev.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'aac.html',// 自定义创建的名字
+            template: './src/index.html'// 根据自定义模版创建
+        })
+    ]
 }
